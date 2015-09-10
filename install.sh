1766ffc3c07e7ec37bb80f76e5fb6ff429988466
Xuyuan Pang <xuyuanp@gmail.com>
2015-09-10 13:58:07 +0800

    Install gitconfig

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 NOW=`date "+%m%d%H%M%Y"`
 
-echo "config tmux:"
+echo "Config tmux:"
 if [ -e $HOME/.tmux.conf ]; then
     echo ".tmux.conf already exists"
     echo "Backup .tmux.conf to .tmux.conf.bak.$NOW"
@@ -10,6 +10,16 @@ if [ -e $HOME/.tmux.conf ]; then
 fi
 echo "Link tmux.conf"
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
+echo "Done."
+
+echo "Config git"
+if [ -e $HOME/.gitconfig ]; then
+    echo ".gitconfig already exists"
+    echo "Backup .gitconfig to .gitconfig.bak.$NOW"
+    mv $HOME/.gitconfig $HOME/.gitconfig.bak.$NOW
+fi
+echo "Link gitconfig"
+ln -s $PWD/gitconfig $HOME/.gitconfig
 echo "Done."
 
 echo "config vim:"
