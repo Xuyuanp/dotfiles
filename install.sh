b8e5876355db647775ddaf750cff1d69257a3cf4
Xuyuan Pang <xuyuanp@gmail.com>
2015-09-10 16:42:14 +0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,65 @@
 #! /usr/bin/env sh
 
-NOW=`date "+%m%d%H%M%Y"`
+export NOW=`date "+%m%d%H%M%Y"`
 
-echo "Config tmux:"
-if [ -e $HOME/.tmux.conf ]; then
-    echo ".tmux.conf already exists"
-    echo "Backup .tmux.conf to .tmux.conf.bak.$NOW"
-    mv $HOME/.tmux.conf $HOME/.tmux.conf.bak.$NOW
+info() {
+    echo "=> $1"
+}
+
+config_vim() {
+    info "Config vim:"
+    curl https://raw.githubusercontent.com/Xuyuanp/vimrc/master/install.sh | sh
+    info "Done."
+}
+
+config_git() {
+    info "Config git"
+    if [ -e $HOME/.gitconfig ]; then
+        info ".gitconfig already exists"
+        info "Backup .gitconfig to .gitconfig.bak.$NOW"
+        mv $HOME/.gitconfig $HOME/.gitconfig.bak.$NOW
+    fi
+    info "Link gitconfig"
+    ln -s $PWD/gitconfig $HOME/.gitconfig
+    info "Done."
+}
+
+config_tmux() {
+    info "Config tmux:"
+    if [ -e $HOME/.tmux.conf ]; then
+        info ".tmux.conf already exists"
+        info "Backup .tmux.conf to .tmux.conf.bak.$NOW"
+        mv $HOME/.tmux.conf $HOME/.tmux.conf.bak.$NOW
+    fi
+    info "Link tmux.conf"
+    ln -s $PWD/tmux.conf $HOME/.tmux.conf
+    info "Done."
+}
+
+config_all() {
+    config_git
+    config_tmux
+    config_vim
+}
+
+if [ $# -eq 0 ] || [ "$1" =  "all" ]; then
+    config_all
+else
+    for name in $*; do
+        case "$name" in
+            vim)
+                config_vim
+                ;;
+            tmux)
+                config_tmux
+                ;;
+            git)
+                config_git
+                ;;
+            *)
+                info "$name Didn't match anything"
+        esac
+    done
 fi
-echo "Link tmux.conf"
-ln -s $PWD/tmux.conf $HOME/.tmux.conf
-echo "Done."
 
-echo "Config git"
-if [ -e $HOME/.gitconfig ]; then
-    echo ".gitconfig already exists"
-    echo "Backup .gitconfig to .gitconfig.bak.$NOW"
-    mv $HOME/.gitconfig $HOME/.gitconfig.bak.$NOW
-fi
-echo "Link gitconfig"
-ln -s $PWD/gitconfig $HOME/.gitconfig
-echo "Done."
-
-echo "config vim:"
-curl https://raw.githubusercontent.com/Xuyuanp/vimrc/master/install.sh | sh
-echo "Done."
+unset NOW
