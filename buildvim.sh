f6c17a79ed534d821ea5d92f271486f6089ba736
Xuyuan Pang <xuyuanp@gmail.com>
2015-09-11 16:53:57 +0800

    RM cd ~

--- buildvim.sh
+++ buildvim.sh
@@ -16,7 +16,6 @@ sudo mkdir /usr/include/lua5.1/lib
 sudo ln -s /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
 sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/include/lua5.1/lib/liblua.so
  
-cd ~
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxf vim-7.4.tar.bz2
 cd vim74
