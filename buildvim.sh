111c15353b2cc19f35629696900e6249912c6302
Xuyuan Pang <xuyuanp@gmail.com>
2016-08-31 15:56:56 +0800

    Uninstall vim finally

--- buildvim.sh
+++ buildvim.sh
@@ -2,18 +2,14 @@
 
 # for linux
 sudo apt-get build-dep
- 
+
 sudo apt-get install -y lua5.1 liblua5.1-dev python-dev ruby-dev libperl-dev mercurial libncurses5-dev
 
-sudo rm -rf /usr/local/share/vim
-
-sudo rm /usr/bin/vim
- 
 sudo mkdir /usr/include/lua5.1/include
 sudo mkdir /usr/include/lua5.1/lib
 sudo ln -s /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
 sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.1.so /usr/include/lua5.1/lib/liblua.so
- 
+
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
 tar jxf vim-7.4.tar.bz2
 cd vim74
@@ -28,9 +24,12 @@ cd vim74
             --enable-gui=auto \
             --enable-fail-if-missing \
             --with-lua-prefix=/usr/include/lua5.1 \
-            --enable-cscope 
-make 
+            --enable-cscope
 
+make
+
+sudo rm -rf /usr/local/share/vim
+sudo rm /usr/bin/vim
 sudo apt-get remove -y --purge vim vim-runtime vim-gnome vim-tiny vim-common vim-gui-common
 
 sudo make install
