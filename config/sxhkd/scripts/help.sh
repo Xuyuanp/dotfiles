625f7f3c324d5c7d25ff3acef34a821eedcf8465
Xuyuan Pang <xuyuanp@gmail.com>
2022-03-23 22:24:48 +0800

    feat(bspwm): merged from tusimple

--- config/sxhkd/scripts/help.sh
+++ config/sxhkd/scripts/help.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc{,.common} |
+awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
     rofi -dmenu -i -markup-rows -no-show-icons -theme ~/.config/bspwm/rofi/themes/keybindings.rasi
