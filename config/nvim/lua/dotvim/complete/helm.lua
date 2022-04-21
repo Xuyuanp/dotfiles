8a670203960fac1149407f3d1a35a49f05a18ae4
Xuyuan Pang <xuyuanp@gmail.com>
2022-04-21 23:05:44 +0800

    feat(nvim): improved cmp config

---
+++ config/nvim/lua/dotvim/complete/helm.lua
@@ -0,0 +1,136 @@
+local a = require('dotvim.util.async')
+local uv = a.uv()
+local api = a.api
+
+local yaml = require('lyaml')
+
+local Chart = {}
+
+function Chart.new(root)
+    local chart = setmetatable({ root = root }, { __index = Chart })
+    chart:load()
+    return chart
+end
+
+local function load_yaml(path)
+    local err, data = uv.read_file(path)
+    assert(not err, err)
+    return yaml.load(data)
+end
+
+function Chart:load()
+    -- local helpers_file = self.root .. '/templates/_helpers.tpl'
+    local chart_file = self.root .. '/Chart.yaml'
+    local values_file = self.root .. '/values.yaml'
+
+    self.values = load_yaml(values_file)
+    self.meta = load_yaml(chart_file)
+end
+
+function Chart:complete(prefix)
+    local obj
+
+    if vim.startswith(prefix, '.Values.') then
+        obj = self.values
+    elseif vim.startswith(prefix, '.Chart.') then
+        obj = self.meta
+    else
+        return
+    end
+
+    local keys = vim.split(prefix, '.', { plain = true })
+
+    for i = 3, #keys - 1 do
+        obj = obj[keys[i]]
+        if not obj then
+            return {}
+        end
+    end
+
+    return obj
+end
+
+local fnamemodify = vim.fn.fnamemodify
+
+local function upper_dir(path)
+    return fnamemodify(path, ':h')
+end
+
+local function find_root_path(path)
+    while path ~= '/' do
+        path = upper_dir(path)
+        if vim.endswith(path, 'templates') then
+            return upper_dir(path)
+        end
+    end
+end
+
+local source = {}
+
+function source.new()
+    local self = setmetatable({
+        charts = setmetatable({}, {
+            __index = function(t, root)
+                t[root] = Chart.new(root)
+                return t[root]
+            end,
+        }),
+    }, { __index = source })
+    return self
+end
+
+function source:get_trigger_characters()
+    return { '.' }
+end
+
+local function get_prefix(str)
+    local fields = vim.split(str, ' ', { plain = true })
+    return fields[#fields]
+end
+
+source.complete = a.wrap(function(self, params, callback)
+    local ctx = params.context
+
+    if not string.find(ctx.cursor_before_line, '{{') then
+        callback({})
+        return
+    end
+
+    local path = api.nvim_buf_get_name(ctx.bufnr)
+    local root = find_root_path(path)
+    if not root then
+        callback({})
+        return
+    end
+
+    local chart = self.charts[root]
+
+    local prefix = get_prefix(ctx.cursor_before_line)
+
+    if prefix == '.' then
+        callback({
+            { label = 'Values' },
+            { label = 'Chart' },
+        })
+        return
+    end
+
+    local obj = chart:complete(prefix)
+
+    if type(obj) == 'table' then
+        if not vim.tbl_islist(obj) then
+            local items = {}
+            for key, _ in pairs(obj) do
+                table.insert(items, { label = key })
+            end
+            callback(items)
+            return
+        end
+    end
+
+    callback({})
+end)
+
+return {
+    new = source.new,
+}
--- config/nvim/lua/dotvim/complete/init.lua
+++ config/nvim/lua/dotvim/complete/init.lua
@@ -146,11 +146,11 @@ function M.setup()
         })
     end
 
-    cmp.register_source('helm', require('dotvim.cmp.helm'))
+    cmp.register_source('helm', require('dotvim.complete.helm').new())
     cmp.setup.filetype('helm', {
         sources = cmp.config.sources({
-            { name = 'helm' }
-        })
+            { name = 'helm' },
+        }),
     })
 
     local autopair_cmp = vim.F.npcall(require, 'nvim-autopairs.completion.cmp')
