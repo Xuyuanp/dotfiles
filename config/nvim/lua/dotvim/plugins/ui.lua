cb7393a1b08e5d2c95677e3f1bafb0c4f476a888
Xuyuan Pang <xuyuanp@gmail.com>
2022-05-10 19:10:30 +0800

    feat(nvim): add hlslens

--- config/nvim/lua/dotvim/plugins/ui.lua
+++ config/nvim/lua/dotvim/plugins/ui.lua
@@ -462,4 +462,18 @@ return {
             })
         end,
     },
+
+    {
+        'kevinhwang91/nvim-hlslens',
+        config = function()
+            local kopts = { noremap = true, silent = true }
+
+            vim.keymap.set('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.keymap.set('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.keymap.set('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.keymap.set('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.keymap.set('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+            vim.keymap.set('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+        end,
+    },
 }
