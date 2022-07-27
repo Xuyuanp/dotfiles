bddcdbb4dc3dc91fe73a65109229d3050d9945f0
Xuyuan Pang <xuyuanp@gmail.com>
2022-07-27 19:49:02 +0800

    style(nvim): format python script

--- config/nvim/scripts/nerdfonts.py
+++ config/nvim/scripts/nerdfonts.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    rsp = requests.get('https://www.nerdfonts.com/cheat-sheet')
-    soup = BeautifulSoup(rsp.text, 'html.parser')
 
-    columns = soup.find_all('div', attrs={'class': 'column'})
-    icons = [(
-        col.find('div', attrs={'class': 'codepoint'}).text,
-        col.find('div', attrs={'class': 'class-name'}).text,
-    ) for col in columns]
+def main():
+    os.getenv("")
+    rsp = requests.get("https://www.nerdfonts.com/cheat-sheet")
+    soup = BeautifulSoup(rsp.text, "html.parser")
+
+    columns = soup.find_all("div", attrs={"class": "column"})
+
+    icons = [
+        (
+            col.find("div", attrs={"class": "codepoint"}).text,
+            col.find("div", attrs={"class": "class-name"}).text,
+        )
+        for col in columns
+    ]
 
     for code, name in icons:
-        print(f'{code} {chr(int(code, 16))} {name}')
+        print(f"{code} {chr(int(code, 16))} {name}")
 
 
 if __name__ == "__main__":
