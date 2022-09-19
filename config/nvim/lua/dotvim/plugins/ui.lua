8209cc429660f2d46cd670bbfcc99a06ee6e503c
Xuyuan Pang <xuyuanp@gmail.com>
2022-09-19 13:15:47 +0800

    feat(nvim): added todo-comments plugin

--- config/nvim/lua/dotvim/plugins/ui.lua
+++ config/nvim/lua/dotvim/plugins/ui.lua
@@ -504,4 +504,50 @@ return {
             require('hlargs').setup({})
         end,
     },
+
+    {
+        'folke/todo-comments.nvim',
+        requires = 'plenary',
+        config = function()
+            require('todo-comments').setup({
+                keywords = {
+                    FIX = {
+                        icon = ' ', -- icon used for the sign, and in search results
+                        color = 'error', -- can be a hex color, or a named color (see below)
+                        alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+                        -- signs = false, -- configure signs for some keywords individually
+                    },
+                    TODO = { icon = ' ', color = 'info' },
+                    HACK = { icon = ' ', color = 'warning' },
+                    WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+                    PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+                    NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+                    TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+                },
+                highlight = {
+                    before = '', -- "fg" or "bg" or empty
+                    keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+                    after = 'fg', -- "fg" or "bg" or empty
+                    pattern = [[.*<(KEYWORDS):?\s+]], -- pattern or table of patterns, used for highlightng (vim regex)
+                    comments_only = true, -- uses treesitter to match keywords in comments only
+                    max_line_len = 400, -- ignore lines longer than this
+                    exclude = {}, -- list of file types to exclude highlighting
+                },
+                search = {
+                    command = 'rg',
+                    args = {
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                    },
+                    -- regex that will be used to match keywords.
+                    -- don't replace the (KEYWORDS) placeholder
+                    -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+                    pattern = [[\b(KEYWORDS):?\b]], -- match without the extra colon. You'll likely get false positives
+                },
+            })
+        end,
+    },
 }
