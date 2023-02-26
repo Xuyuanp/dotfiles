0089c4ce14b1d43e890fd2b225cf78dcf6620131
Xuyuan Pang <xuyuanp@gmail.com>
2023-02-26 19:52:56 +0800

    feat(nvim): add git conflict plugin

--- config/nvim/lua/dotvim/plugins/ui.lua
+++ config/nvim/lua/dotvim/plugins/ui.lua
@@ -232,19 +232,15 @@ return {
                 -- Enable by default. You can disable and use :NvimContextVtToggle to maually enable.
                 -- Default: true
                 enabled = true,
-
                 -- Disable virtual text for given filetypes
                 -- Default: { 'markdown' }
                 disable_ft = { 'markdown' },
-
                 -- Disable display of virtual text below blocks for indentation based languages like Python
                 -- Default: false
                 disable_virtual_lines = false,
-
                 -- Same as above but only for spesific filetypes
                 -- Default: {}
                 disable_virtual_lines_ft = { 'yaml', 'python' },
-
                 -- How many lines required after starting position to show virtual text
                 -- Default: 1 (equals two lines total)
                 min_rows = 80,
@@ -566,5 +562,23 @@ return {
 
     {
         'eandrju/cellular-automaton.nvim',
+    },
+
+    {
+        'akinsho/git-conflict.nvim',
+        tags = '*',
+        config = function()
+            require('git-conflict').setup({
+                default_mappings = {
+                    ours = '<leader>co',
+                    theirs = '<leader>ct',
+                    none = '<leader>c0',
+                    both = '<leader>cb',
+                    next = ']x',
+                    prev = '[x',
+                },
+                default_commands = true,
+            })
+        end,
     },
 }
