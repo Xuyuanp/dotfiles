935353a6a6925df4bebaebc183e136d54a16cdaf
Xuyuan Pang <xuyuanp@gmail.com>
2023-03-19 17:07:40 +0800

    feat(nvim): lazy load dressing

--- config/nvim/lua/dotvim/plugins/ui.lua
+++ config/nvim/lua/dotvim/plugins/ui.lua
@@ -282,7 +282,7 @@ return {
         },
         init = function()
             vim.api.nvim_create_autocmd('FileType', {
-                pattern = { 'help', 'Yanil', 'lazy', 'mason' },
+                pattern = { 'help', 'Yanil', 'lazy', 'mason', 'terminal' },
                 callback = function()
                     vim.b.miniindentscope_disable = true
                 end,
@@ -326,7 +326,19 @@ return {
 
     {
         'stevearc/dressing.nvim',
-        event = { 'VeryLazy' },
+        lazy = true,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require('lazy').load({ plugins = { 'dressing.nvim' } })
+                return vim.ui.select(...)
+            end
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.input = function(...)
+                require('lazy').load({ plugins = { 'dressing.nvim' } })
+                return vim.ui.input(...)
+            end
+        end,
         config = function()
             require('dressing').setup({
                 input = {
@@ -532,7 +544,8 @@ return {
 
     {
         'folke/drop.nvim',
-        event = { 'BufReadPost', 'BufNewFile' },
+        event = { 'CursorHold' },
+        cmd = { 'DropShow' },
         config = function()
             require('drop').setup({
                 theme = 'leaves',
