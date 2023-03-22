93c016cdec1ae3912d99e045f1aef607b8e8371f
Xuyuan Pang <xuyuanp@gmail.com>
2023-03-22 14:33:41 +0800

    feat(nvim): config null-ls

---
+++ config/nvim/lua/dotvim/config/lsp/null.lua
@@ -0,0 +1,65 @@
+local M = {}
+
+local function on_lsp_attach(client, bufnr)
+    local support_formatting = client.supports_method('textDocument/formatting')
+    if not support_formatting then
+        return
+    end
+
+    if client.name == 'null-ls' then
+        vim.api.nvim_create_autocmd('BufWritePost', {
+            buffer = bufnr,
+            desc = 'Formatting on save by null-ls',
+            callback = function()
+                vim.lsp.buf.format({
+                    name = 'null-ls',
+                    bufnr = bufnr,
+                })
+            end,
+        })
+    elseif not vim.b[bufnr].lsp_disable_auto_format then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = bufnr,
+            desc = 'Formatting on save by lsp ' .. client.name,
+            callback = function()
+                vim.lsp.buf.format({
+                    name = client.name,
+                    bufnr = bufnr,
+                })
+            end,
+        })
+    end
+end
+
+function M.setup()
+    local null_ls = require('null-ls')
+
+    null_ls.setup({
+        sources = {
+            -- formatting
+            null_ls.builtins.formatting.stylua,
+            null_ls.builtins.formatting.prettierd, -- graphql
+            null_ls.builtins.formatting.black, -- python
+            null_ls.builtins.formatting.buf, -- proto
+            null_ls.builtins.formatting.taplo, -- toml
+            null_ls.builtins.formatting.jq, -- json
+            null_ls.builtins.formatting.goimports_reviser.with({
+                generator_opts = {
+                    command = 'goimports-reviser',
+                    args = { '-set-alias', '-use-cache', '-rm-unused', '-output', 'stdout', '$FILENAME' },
+                    to_stdin = true,
+                },
+            }),
+
+            -- diagnostics
+            null_ls.builtins.diagnostics.codespell,
+
+            -- code_actions
+            null_ls.builtins.code_actions.gitsigns,
+            null_ls.builtins.code_actions.gomodifytags,
+        },
+    })
+    require('dotvim.util').on_lsp_attach(on_lsp_attach, { desc = 'set auto formatting trigger' })
+end
+
+return M
--- config/nvim/lua/dotvim/plugins/lsp.lua
+++ config/nvim/lua/dotvim/plugins/lsp.lua
@@ -268,49 +268,7 @@ return {
         'jose-elias-alvarez/null-ls.nvim',
         lazy = true,
         config = function()
-            local null_ls = require('null-ls')
-
-            null_ls.setup({
-                sources = {
-                    -- formatting
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.prettierd, -- graphql
-                    null_ls.builtins.formatting.black, -- python
-                    null_ls.builtins.formatting.buf, -- proto
-                    null_ls.builtins.formatting.taplo, -- toml
-                    null_ls.builtins.formatting.jq, -- json
-                    null_ls.builtins.formatting.goimports_reviser.with({
-                        generator_opts = {
-                            command = 'goimports-reviser',
-                            args = { '-set-alias', '-use-cache', '-rm-unused', '-output', 'stdout', '$FILENAME' },
-                            to_stdin = true,
-                        },
-                    }),
-
-                    -- diagnostics
-                    null_ls.builtins.diagnostics.codespell,
-
-                    -- code_actions
-                    null_ls.builtins.code_actions.gitsigns,
-                    null_ls.builtins.code_actions.gomodifytags,
-                },
-            })
-            require('dotvim.util').on_lsp_attach(function(client, bufnr)
-                if client.name ~= 'null-ls' then
-                    return
-                end
-
-                vim.api.nvim_create_autocmd('BufWritePre', {
-                    buffer = bufnr,
-                    desc = 'Formatting on save by null-ls',
-                    callback = function()
-                        vim.lsp.buf.format({
-                            name = 'null-ls',
-                            bufnr = bufnr,
-                        })
-                    end,
-                })
-            end, { desc = 'set auto format trigger' })
+            require('dotvim.config.lsp.null').setup()
         end,
     },
 }
