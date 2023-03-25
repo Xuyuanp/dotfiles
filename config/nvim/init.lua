e9121130bf4ae127d375f060f11f8bb28048ef40
Xuyuan Pang <xuyuanp@gmail.com>
2023-03-25 21:53:52 +0800

    feat(nvim): removed pprint as it was replaced by pretty_print

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,10 +2,6 @@ if not vim.fn.has('nvim-0.8') then
     error('require nvim >= 0.8')
 end
 
-_G.pprint = function(obj)
-    print(vim.inspect(obj))
-end
-
 require('dotvim.settings').setup()
 require('dotvim.lazy').setup()
 require('dotvim.mappings').setup()
