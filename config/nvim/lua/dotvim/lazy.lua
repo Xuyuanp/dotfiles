9c106a0c63b8dca916cb568b872d4f2df1df910a
Xuyuan Pang <xuyuanp@gmail.com>
2023-03-28 00:14:52 +0800

    feat(nvim): move neochat out of the tree

--- config/nvim/lua/dotvim/lazy.lua
+++ config/nvim/lua/dotvim/lazy.lua
@@ -27,6 +27,33 @@ local function get_plugins()
     return {
         { 'folke/lazy.nvim', version = '*' },
         { import = 'dotvim.plugins' },
+        {
+            'Xuyuanp/neochat.nvim',
+            dev = vim.fn.exists('~/workspace/neovim/neochat.nvim'),
+            build = function()
+                vim.fn.system({
+                    'pip',
+                    'install',
+                    '-U',
+                    'openai',
+                })
+            end,
+            keys = {
+                {
+                    '<A-g>',
+                    require('dotvim.util').lazy_require('neochat').toggle,
+                    mode = { 'n', 'i' },
+                    desc = 'toggle neochat',
+                    noremap = false,
+                },
+            },
+            dependencies = {
+                'MunifTanjim/nui.nvim',
+            },
+            config = function()
+                require('neochat').setup({})
+            end,
+        },
     }
 end
 
@@ -40,6 +67,10 @@ function M.setup()
         install = {
             missing = true,
             colorscheme = { 'kanagawa' },
+        },
+        dev = {
+            path = '~/workspace/neovim',
+            fallback = true,
         },
         performance = {
             rtp = {
