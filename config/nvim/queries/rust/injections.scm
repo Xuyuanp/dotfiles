35244aa116bd22ac8969ec3a39f39ef5cc16cdde
Xuyuan Pang <xuyuanp@gmail.com>
2023-04-15 18:38:48 +0800

    feat(nvim): moved the sqlx formatter out of tree

--- config/nvim/queries/rust/injections.scm
+++ config/nvim/queries/rust/injections.scm
@@ -5,31 +5,3 @@
 
     (#eq? @_html_def "html")
 )
-
-(
- (macro_invocation
-  (scoped_identifier
-     path: (identifier) @_path
-     name: (identifier) @_identifier)
-
-  (token_tree (raw_string_literal) @injection.content))
-
- (#eq? @_path "sqlx")
- (#match? @_identifier "^query")
- (#offset! @injection.content 0 3 0 -2)
- (#set! injection.language "sql")
-)
-
-(
- (macro_invocation
-  (scoped_identifier
-     path: (identifier) @_path
-     name: (identifier) @_identifier)
-
-  (token_tree (string_literal) @injection.content))
-
- (#eq? @_path "sqlx")
- (#match? @_identifier "^query")
- (#offset! @injection.content 0 1 0 -1)
- (#set! injection.language "sql")
-)
