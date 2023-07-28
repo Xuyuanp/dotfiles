6b8121026863b5dda07ea6d5d1166c1136a5666b
Xuyuan Pang <xuyuanp@gmail.com>
2023-07-28 22:12:48 +0800

    feat(nvim): upadted ftplugins

---
+++ config/nvim/after/ftplugin/c.lua
@@ -0,0 +1,6 @@
+vim.b.lsp_disable_auto_format = false
+
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.cindent = true
--- config/nvim/after/ftplugin/c.vim
+++
@@ -1,6 +0,0 @@
-let b:lsp_disable_auto_format = v:true
-
-setlocal softtabstop=2
-setlocal tabstop=2
-setlocal shiftwidth=2
-setlocal cindent
