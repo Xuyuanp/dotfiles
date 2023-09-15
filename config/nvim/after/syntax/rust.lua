00a26d3eb09f22c302993c4388e9f375efaeb1ae
Xuyuan Pang <xuyuanp@gmail.com>
2023-09-15 23:02:15 +0800

    feat(nvim): highlight rust mutable var and parameter

---
+++ config/nvim/after/syntax/rust.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_set_hl(0, '@lsp.typemod.parameter.mutable.rust', {
+    bold = true,
+    italic = true,
+    underline = true,
+})
+
+vim.api.nvim_set_hl(0, '@lsp.typemod.variable.mutable.rust', {
+    bold = true,
+    italic = true,
+    underline = true,
+})
