775ecb3eb88bd3fb491172b93629278763d1d9cc
Xuyuan Pang <xuyuanp@gmail.com>
2023-09-17 18:32:11 +0800

    feat(nvim): reorganized plugins

--- config/nvim/lua/dotvim/plugins/tools.lua
+++ config/nvim/lua/dotvim/plugins/tools.lua
@@ -223,46 +223,4 @@ return {
             yank_dry_run = true,
         },
     },
-
-    {
-        'nvim-neotest/neotest',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-treesitter/nvim-treesitter',
-        },
-        config = function()
-            require('dotvim.config.neotest').setup()
-        end,
-    },
-
-    {
-        'nvim-neotest/neotest-go',
-        ft = 'go',
-        config = function()
-            require('neotest').setup({
-                adapters = {
-                    require('neotest-go')({
-                        experimental = {
-                            test_table = true,
-                        },
-                        args = { '-count=1', '-race', '-timeout=60s' },
-                    }),
-                },
-            })
-        end,
-    },
-
-    {
-        'rouge8/neotest-rust',
-        ft = 'rust',
-        config = function()
-            require('neotest').setup({
-                adapters = {
-                    require('neotest-rust')({
-                        args = { '--no-capture' },
-                    }),
-                },
-            })
-        end,
-    },
 }
