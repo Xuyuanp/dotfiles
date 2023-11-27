573242aab7a14e25b4c2f96a04e691be34357713
Xuyuan Pang <xuyuanp@gmail.com>
2023-11-27 17:21:58 +0800

    feat(nvim): improved go kubebuilder marker highlights

--- config/nvim/after/syntax/go.lua
+++ config/nvim/after/syntax/go.lua
@@ -1,3 +1,17 @@
 vim.api.nvim_set_hl(0, '@kubebuilder.go', {
     link = 'Keyword',
 })
+
+for name, link in pairs({
+    rbac = 'Structure',
+    object = 'Structure',
+    subresource = 'Structure',
+    webhook = 'Structure',
+    default = 'Structure',
+    validation = 'Structure',
+    example = 'Structure',
+}) do
+    vim.api.nvim_set_hl(0, '@kubebuilder.' .. name .. '.go', {
+        link = link,
+    })
+end
--- config/nvim/queries/go/highlights.scm
+++ config/nvim/queries/go/highlights.scm
@@ -1,7 +1,51 @@
 ;; inherits: go
 ;; extends
 
+; stupid
+
  ((comment) @kubebuilder
             (#match? @kubebuilder "^//\\+kubebuilder:")
             (#absoffset! @kubebuilder 0 3 0 11)
+            )
+
+ ((comment) @kubebuilder.rbac
+            (#match? @kubebuilder.rbac "^//\\+kubebuilder:rbac:")
+            (#offset! @kubebuilder.rbac 0 15 0 -1)
+            (#absoffset! @kubebuilder.rbac 0 0 0 4)
+            )
+
+ ((comment) @kubebuilder.object
+            (#match? @kubebuilder.object "^//\\+kubebuilder:object:")
+            (#offset! @kubebuilder.object 0 15 0 -1)
+            (#absoffset! @kubebuilder.object 0 0 0 6)
+            )
+
+ ((comment) @kubebuilder.subresource
+            (#match? @kubebuilder.subresource "^//\\+kubebuilder:subresource:")
+            (#offset! @kubebuilder.subresource 0 15 0 -1)
+            (#absoffset! @kubebuilder.subresource 0 0 0 11)
+            )
+
+ ((comment) @kubebuilder.webhook
+            (#match? @kubebuilder.webhook "^//\\+kubebuilder:webhook:")
+            (#offset! @kubebuilder.webhook 0 15 0 -1)
+            (#absoffset! @kubebuilder.webhook 0 0 0 7)
+            )
+
+ ((comment) @kubebuilder.validation
+            (#match? @kubebuilder.validation "^//\\+kubebuilder:validation:")
+            (#offset! @kubebuilder.validation 0 15 0 -1)
+            (#absoffset! @kubebuilder.validation 0 0 0 10)
+            )
+
+ ((comment) @kubebuilder.default
+            (#match? @kubebuilder.default "^//\\+kubebuilder:default:")
+            (#offset! @kubebuilder.default 0 15 0 -1)
+            (#absoffset! @kubebuilder.default 0 0 0 7)
+            )
+
+ ((comment) @kubebuilder.example
+            (#match? @kubebuilder.example "^//\\+kubebuilder:example:")
+            (#offset! @kubebuilder.example 0 15 0 -1)
+            (#absoffset! @kubebuilder.example 0 0 0 7)
             )
