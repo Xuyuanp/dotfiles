f5cd39e6fd97f1275be0907ce90268129272e085
Xuyuan Pang <xuyuanp@gmail.com>
2024-01-04 17:10:43 +0800

    feat(nvim): config diagnostic signs

--- config/nvim/lua/dotvim/commands.lua
+++ config/nvim/lua/dotvim/commands.lua
@@ -2,6 +2,10 @@ local M = {}
 
 function M.setup()
     vim.api.nvim_create_user_command('Q', 'execute("qa!")', {})
+
+    vim.api.nvim_create_user_command('Nerdfonts', function()
+        require('dotvim.util.nerdfonts').pick()
+    end, {})
 end
 
 return M
