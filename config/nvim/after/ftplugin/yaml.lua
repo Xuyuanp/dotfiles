24be9551d580a38a96c80a49e397a4acb92f1512
Xuyuan Pang <xuyuanp@gmail.com>
2024-08-05 23:13:08 +0800

    feat(nvim): improved helm detection

--- config/nvim/after/ftplugin/yaml.lua
+++ config/nvim/after/ftplugin/yaml.lua
@@ -3,27 +3,13 @@ local vim = vim
 local a = require('dotvim.util.async')
 local api = a.api
 
-local fnamemodify = vim.fn.fnamemodify
-
-local function upper_dir(path)
-    return fnamemodify(path, ':h')
-end
-
 local detect_helm_ft = a.wrap(function(bufnr, path)
     if not bufnr or not path then
         return
     end
-
-    while path ~= '/' do
-        path = upper_dir(path)
-        if vim.endswith(path, 'templates') then
-            path = upper_dir(path)
-            local err, stat = a.uv().fs_stat(path .. '/' .. 'Chart.yaml').await()
-            if not err and stat then
-                vim.api.nvim_set_option_value('filetype', 'helm', { buf = bufnr })
-            end
-            return
-        end
+    local root = vim.fs.root(bufnr or path, 'Chart.yaml')
+    if root then
+        vim.api.nvim_set_option_value('filetype', 'helm', { buf = bufnr })
     end
 end)
 
