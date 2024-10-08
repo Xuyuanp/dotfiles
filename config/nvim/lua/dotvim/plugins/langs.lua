7e94a132fbad632ced5565e967c45096412f2455
Xuyuan Pang <xuyuanp@gmail.com>
2024-10-08 11:22:29 +0800

    feat(nvim): added new plugin kulala

--- config/nvim/lua/dotvim/plugins/langs.lua
+++ config/nvim/lua/dotvim/plugins/langs.lua
@@ -117,4 +117,52 @@ return {
         opts = {},
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
     },
+    {
+        'mistweaverco/kulala.nvim',
+        ft = 'http',
+        init = function()
+            vim.filetype.add({
+                extension = {
+                    http = 'http',
+                },
+            })
+        end,
+        opts = {
+            winbar = true,
+        },
+        config = function(_, opts)
+            local kulala = require('kulala')
+            kulala.setup(opts)
+
+            local a = require('dotvim.util.async')
+
+            local show_menu = a.wrap(function()
+                local actions = {
+                    'run',
+                    'run_all',
+                    'replay',
+                    'inspect',
+                    'show_stats',
+                    'copy',
+                    'from_curl',
+                    'toggle_view',
+                    'jump_prev',
+                    'jump_next',
+                }
+                local action = a.ui.select(actions, { prompt = 'Select Action:' }).await()
+                if not action then
+                    return
+                end
+                a.schedule().await()
+                kulala[action]()
+            end)
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'http',
+                callback = function(ev)
+                    vim.keymap.set('n', '<A-m>', show_menu, { buffer = ev.buf, remap = true, desc = '[Kulala] menu' })
+                end,
+            })
+        end,
+    },
 }
