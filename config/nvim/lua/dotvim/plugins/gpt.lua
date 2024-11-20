7eede104f942aaa2cabfefdec16451d60f345d8d
Xuyuan Pang <xuyuanp@gmail.com>
2024-11-20 14:41:50 +0800

    feat(nvim): added plugin codecompanion

--- config/nvim/lua/dotvim/plugins/gpt.lua
+++ config/nvim/lua/dotvim/plugins/gpt.lua
@@ -88,4 +88,22 @@ return {
             require('dotvim.config.howto').setup()
         end,
     },
+
+    {
+        'olimorris/codecompanion.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-treesitter/nvim-treesitter',
+            'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
+            'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
+            { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
+            { 'stevearc/dressing.nvim' }, -- Optional: Improves `vim.ui.select`
+        },
+        cmd = {
+            'CodeCompanion',
+            'CodeCompanionChat',
+            'CodeCompanionActions',
+        },
+        opts = {},
+    },
 }
