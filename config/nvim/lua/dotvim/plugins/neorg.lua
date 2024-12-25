0e173cc15de59a1f9923e2804278e7539c365d07
Xuyuan Pang <xuyuanp@gmail.com>
2024-12-25 12:43:18 +0800

    feat(nvim): config completion of neorg

--- config/nvim/lua/dotvim/plugins/neorg.lua
+++ config/nvim/lua/dotvim/plugins/neorg.lua
@@ -10,28 +10,52 @@ return {
         ft = { 'norg' },
         dependencies = {
             'vhyrro/luarocks.nvim',
+            'benlubas/neorg-interim-ls',
         },
-        config = function()
-            require('neorg').setup({
-                load = {
-                    ['core.defaults'] = {}, -- Loads default behaviour
-                    ['core.keybinds'] = {
-                        config = {
-                            hook = function(keybinds)
-                                keybinds.remap_event('norg', 'n', '<leader>mcb', 'core.looking-glass.magnify-code-block')
-                            end,
-                        },
+        opts = {
+            load = {
+                ['core.defaults'] = {}, -- Loads default behaviour
+                ['core.keybinds'] = {
+                    config = {
+                        hook = function(keybinds)
+                            keybinds.remap_event('norg', 'n', '<leader>mcb', 'core.looking-glass.magnify-code-block')
+                        end,
                     },
-                    ['core.concealer'] = {}, -- Adds pretty icons to your documents
-                    ['core.dirman'] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = '~/notes',
-                            },
+                },
+                ['core.concealer'] = {}, -- Adds pretty icons to your documents
+                ['core.dirman'] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = '~/notes',
                         },
                     },
                 },
-            })
-        end,
+            },
+        },
+    },
+
+    {
+        'nvim-neorg/neorg',
+        optional = true,
+        dependencies = {
+            'benlubas/neorg-interim-ls',
+        },
+        opts = {
+            load = {
+                ['core.completion'] = {
+                    config = { engine = { module_name = 'external.lsp-completion' } },
+                },
+                ['external.interim-ls'] = {
+                    config = {
+                        completion_provider = {
+                            enable = true,
+                            documentation = true,
+                            -- Try to complete categories provided by Neorg Query. Requires `benlubas/neorg-query`
+                            categories = false,
+                        },
+                    },
+                },
+            },
+        },
     },
 }
