a29004d9ba2d21cdf5620ed60b70850f65988284
Xuyuan Pang <xuyuanp@gmail.com>
2024-12-26 19:32:47 +0800

    feat(nvim): new command DiffOrig

--- config/nvim/lua/dotvim/commands.lua
+++ config/nvim/lua/dotvim/commands.lua
@@ -6,6 +6,12 @@ function M.setup()
     vim.api.nvim_create_user_command('Nerdfonts', function()
         require('dotvim.util.nerdfonts').pick()
     end, {})
+
+    vim.api.nvim_create_user_command(
+        'DiffOrig',
+        'vert new | set buftype=nofile | read ++edit # | 0d_ | diffthis | wincmd p | diffthis',
+        { desc = 'see :h DiffOrig' }
+    )
 end
 
 return M
