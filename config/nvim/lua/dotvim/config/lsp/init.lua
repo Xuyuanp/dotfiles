21a8c68ceb9d5f64ad91315572a61828529eae04
Xuyuan Pang <xuyuanp@gmail.com>
2024-12-29 22:11:00 +0800

    feat(nvim): cleanup lsp config

--- config/nvim/lua/dotvim/config/lsp/init.lua
+++ config/nvim/lua/dotvim/config/lsp/init.lua
@@ -10,19 +10,6 @@ local function make_capabilities()
     return default_capabilities
 end
 
-local function make_on_new_config(opts)
-    opts = opts or {}
-    return function(new_config)
-        local on_attach_origin = new_config.on_attach or function() end
-        new_config.on_attach = function(client, ...)
-            if opts.disable_hover then
-                client.server_capabilities.hoverProvider = false
-            end
-            on_attach_origin(client, ...)
-        end
-    end
-end
-
 local langs = {
     gopls = {
         filetypes = { 'go', 'gomod', 'gotmpl', 'helm' },
@@ -90,20 +77,9 @@ local langs = {
             },
         },
     },
-    sourcery = {
-        on_new_config = make_on_new_config({ disable_hover = true }),
-    },
-    ruff_lsp = {
-        on_new_config = make_on_new_config({ disable_hover = true }),
-    },
     clangd = {
         -- disable clangd for proto files
         filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-        capabilities = {
-            -- suppress warning: multiple different client offset_encodings detected for buffer, this is not supported yet
-            -- see: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428#issuecomment-997226723
-            offsetEncoding = { 'utf-16' },
-        },
     },
 }
 
@@ -126,11 +102,8 @@ function M.setup()
         vim.lsp.enable(server_name)
     end
     local oldstyle_setup = function(server_name)
-        local cfg = vim.deepcopy(default_config)
-        if langs[server_name] then
-            cfg = vim.tbl_deep_extend('force', cfg, langs[server_name])
-        end
         local lspconfig = require('lspconfig')
+        local cfg = vim.tbl_deep_extend('force', default_config, langs[server_name] or {})
         lspconfig[server_name].setup(cfg)
     end
 
