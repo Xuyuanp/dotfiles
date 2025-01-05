0ecc055e31afac4e65c796658373e2ffbced6c86
Xuyuan Pang <xuyuanp@gmail.com>
2025-01-05 16:20:34 +0800

    feat(nvim): updated treesitter config

--- config/nvim/lua/dotvim/config/treesitter.lua
+++ config/nvim/lua/dotvim/config/treesitter.lua
@@ -32,79 +32,11 @@ function M.register_custom_directives()
     end
 end
 
-function M.setup()
+function M.setup(opts)
     M.register_custom_directives()
 
     local ts_configs = require('nvim-treesitter.configs')
-
-    ts_configs.setup({
-        ensure_installed = {},
-        ignore_install = {},
-        sync_install = false,
-        auto_install = false,
-        modules = {},
-        highlight = {
-            enable = true, -- false will disable the whole extension
-        },
-        incremental_selection = {
-            enable = true,
-        },
-        indent = {
-            enable = true,
-            disable = { 'python', 'yaml', 'helm' },
-        },
-        textobjects = {
-            select = {
-                enable = true,
-
-                -- Automatically jump forward to textobj, similar to targets.vim
-                lookahead = true,
-
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ['af'] = '@function.outer',
-                    ['if'] = '@function.inner',
-                    ['ac'] = '@class.outer',
-                    -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                    -- nvim_buf_set_keymap) which plugins like which-key display
-                    ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-                    -- You can also use captures from other query groups like `locals.scm`
-                    ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
-                },
-                -- You can choose the select mode (default is charwise 'v')
-                --
-                -- Can also be a function which gets passed a table with the keys
-                -- * query_string: eg '@function.inner'
-                -- * method: eg 'v' or 'o'
-                -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                -- mapping query_strings to modes.
-                selection_modes = {
-                    ['@parameter.outer'] = 'v', -- charwise
-                    ['@function.outer'] = 'V', -- linewise
-                    ['@class.outer'] = '<c-v>', -- blockwise
-                },
-                -- If you set this to `true` (default is `false`) then any textobject is
-                -- extended to include preceding or succeeding whitespace. Succeeding
-                -- whitespace has priority in order to act similarly to eg the built-in
-                -- `ap`.
-                --
-                -- Can also be a function which gets passed a table with the keys
-                -- * query_string: eg '@function.inner'
-                -- * selection_mode: eg 'v'
-                -- and should return true of false
-                include_surrounding_whitespace = false,
-            },
-        },
-        textsubjects = {
-            enable = true,
-            prev_selection = ',', -- (Optional) keymap to select the previous selection
-            keymaps = {
-                ['.'] = 'textsubjects-smart',
-                [';'] = 'textsubjects-container-outer',
-                ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers (classes, functions, etc.)' },
-            },
-        },
-    })
+    ts_configs.setup(opts)
 end
 
 return M
--- config/nvim/lua/dotvim/plugins/treesitter.lua
+++ config/nvim/lua/dotvim/plugins/treesitter.lua
@@ -1,20 +1,113 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
-            'RRethy/nvim-treesitter-textsubjects',
-            {
-                'JoosepAlviste/nvim-ts-context-commentstring',
-                init = function()
-                    vim.g.skip_ts_context_commentstring_module = true
-                end,
-            },
-        },
         build = ':TSUpdate',
         event = 'VeryLazy',
-        config = function()
-            require('dotvim.config.treesitter').setup()
+        opts = {
+            ensure_installed = {},
+            ignore_install = {},
+            sync_install = false,
+            auto_install = false,
+            modules = {},
+            highlight = {
+                enable = true, -- false will disable the whole extension
+            },
+            incremental_selection = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
+                disable = { 'python', 'yaml', 'helm' },
+            },
+        },
+        config = function(_, opts)
+            require('dotvim.config.treesitter').setup(opts)
         end,
+    },
+
+    {
+
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        event = 'VeryLazy',
+        dependencies = {
+            {
+                'nvim-treesitter/nvim-treesitter',
+                opts = {
+                    textobjects = {
+                        select = {
+                            enable = true,
+
+                            -- Automatically jump forward to textobj, similar to targets.vim
+                            lookahead = true,
+
+                            keymaps = {
+                                -- You can use the capture groups defined in textobjects.scm
+                                ['af'] = '@function.outer',
+                                ['if'] = '@function.inner',
+                                ['ac'] = '@class.outer',
+                                -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                                -- nvim_buf_set_keymap) which plugins like which-key display
+                                ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
+                                -- You can also use captures from other query groups like `locals.scm`
+                                ['as'] = { query = '@scope', query_group = 'locals', desc = 'Select language scope' },
+                            },
+                            -- You can choose the select mode (default is charwise 'v')
+                            --
+                            -- Can also be a function which gets passed a table with the keys
+                            -- * query_string: eg '@function.inner'
+                            -- * method: eg 'v' or 'o'
+                            -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                            -- mapping query_strings to modes.
+                            selection_modes = {
+                                ['@parameter.outer'] = 'v', -- charwise
+                                ['@function.outer'] = 'V', -- linewise
+                                ['@class.outer'] = '<c-v>', -- blockwise
+                            },
+                            -- If you set this to `true` (default is `false`) then any textobject is
+                            -- extended to include preceding or succeeding whitespace. Succeeding
+                            -- whitespace has priority in order to act similarly to eg the built-in
+                            -- `ap`.
+                            --
+                            -- Can also be a function which gets passed a table with the keys
+                            -- * query_string: eg '@function.inner'
+                            -- * selection_mode: eg 'v'
+                            -- and should return true of false
+                            include_surrounding_whitespace = false,
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
+
+        'RRethy/nvim-treesitter-textsubjects',
+        event = 'VeryLazy',
+        dependencies = {
+            {
+                'nvim-treesitter/nvim-treesitter',
+                opts = {
+                    textsubjects = {
+                        enable = true,
+                        prev_selection = ',', -- (Optional) keymap to select the previous selection
+                        keymaps = {
+                            ['.'] = 'textsubjects-smart',
+                            [';'] = 'textsubjects-container-outer',
+                            ['i;'] = { 'textsubjects-container-inner', desc = 'Select inside containers (classes, functions, etc.)' },
+                        },
+                    },
+                },
+            },
+        },
+    },
+
+    {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        event = 'VeryLazy',
+        init = function()
+            vim.g.skip_ts_context_commentstring_module = true
+        end,
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        opts = {},
     },
 }
