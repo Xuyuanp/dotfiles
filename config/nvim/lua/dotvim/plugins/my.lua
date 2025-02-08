18d32deef9079ec2ef407e413ac1d0f4174549aa
Xuyuan Pang <xuyuanp@gmail.com>
2025-02-08 10:53:06 +0800

    feat(nvim): removed neochat

--- config/nvim/lua/dotvim/plugins/my.lua
+++ config/nvim/lua/dotvim/plugins/my.lua
@@ -1,64 +1,6 @@
 local lazy_require = require('dotvim.util').lazy_require
+
 return {
-    {
-        'Xuyuanp/neochat.nvim',
-        dev = true,
-        build = function()
-            vim.fn.system({
-                'pip',
-                'install',
-                '-U',
-                'openai',
-            })
-        end,
-        keys = {
-            {
-                '<A-g>',
-                lazy_require('neochat').toggle,
-                mode = { 'n', 'i' },
-                desc = '[NeoChat] toggle',
-                noremap = false,
-            },
-        },
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-            'f/awesome-chatgpt-prompts',
-            'nvim-telescope/telescope.nvim',
-        },
-        cmd = 'NeoChatExplainCode',
-        config = function()
-            require('neochat').setup({
-                bot_text = 'Bot:',
-                bot_sign = '',
-                user_text = 'You:',
-                user_sign = '',
-                spinners = 'line',
-                openai = {
-                    chat_completions = {
-                        model = 'deepseek-chat',
-                    },
-                },
-            })
-
-            vim.api.nvim_create_user_command('NeoChatExplainCode', function(args)
-                local ft = vim.bo.filetype
-                if not ft then
-                    return
-                end
-
-                local code_block = vim.api.nvim_buf_get_lines(0, args.line1 - 1, args.line2, false)
-                local input = { 'Explain the following code', '```' .. ft }
-                vim.list_extend(input, code_block)
-                vim.list_extend(input, { '```' })
-
-                require('neochat').open(input)
-            end, {
-                desc = '[NeoChat] explain code',
-                range = true,
-            })
-        end,
-    },
-
     {
         'Xuyuanp/sqlx-rs.nvim',
         ft = { 'rust' },
