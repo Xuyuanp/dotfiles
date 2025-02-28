27ccfbd15817b4c59f122d08d211bfe39152a910
Xuyuan Pang <xuyuanp@gmail.com>
2025-02-28 15:32:50 +0800

    fix(nvim): inline completion params for copilot-ls

--- config/nvim/lua/dotvim/config/coding/inline.lua
+++ config/nvim/lua/dotvim/config/coding/inline.lua
@@ -1,5 +1,6 @@
 ---@class dotvim.config.blink.inline.Opts
 ---@field transform? fun(item: blink.cmp.CompletionItem, ctx: blink.cmp.Context, client_id: integer): blink.cmp.CompletionItem
+---@field new_params? fun(client: vim.lsp.Client, bufnr: integer, params: table): table
 
 ---@module 'blink.cmp'
 ---@class dotvim.config.blink.inline.Source : blink.cmp.Source
@@ -21,6 +22,15 @@ local default_opts = {
         end
         return item
     end,
+    new_params = function(client, _bufnr, params)
+        if client.name == 'copilot-ls' then
+            params.formattingOptions = {
+                tabSize = vim.fn.shiftwidth(),
+                insertSpaces = vim.o.expandtab,
+            }
+        end
+        return params
+    end,
 }
 
 ---@param opts dotvim.config.blink.inline.Opts
@@ -35,40 +45,65 @@ function source:get_trigger_characters()
 end
 
 function source:get_completions(ctx, callback)
-    local params = function(client, _bufnr)
-        local params = vim.lsp.util.make_position_params(0, client.offset_encoding) --[[@as table]]
-        params.context = {
-            triggerKind = ctx.trigger.kind == 'Invoked' and TriggerKind.Invoked or TriggerKind.Automatic,
-        }
-        params.formattingOptions = {
-            tabSize = vim.fn.shiftwidth(),
-            insertSpaces = vim.o.expandtab,
-        }
-        return params
+    local method = vim.lsp.protocol.Methods.textDocument_inlineCompletion
+    local clients = vim.lsp.get_clients({ bufnr = ctx.bufnr, method = method })
+    if not clients or not next(clients) then
+        callback({
+            items = {},
+            is_incomplete_forward = false,
+            is_incomplete_backward = false,
+        })
+        return
     end
+
     local ft = vim.bo[ctx.bufnr].filetype
+
+    ---@param item lsp.InlineCompletionItem
+    ---@return blink.cmp.CompletionItem
+    local function format_item(item)
+        local kind, label
+        local text = item.insertText
+        if type(text) == 'string' then
+            kind = vim.lsp.protocol.CompletionItemKind.Text
+        else
+            text = text.value
+            kind = vim.lsp.protocol.CompletionItemKind.Snippet
+        end
+        label = vim.trim(text)
+        local documentation = string.format('```%s\n%s\n```\n', ft, label)
+        return {
+            kind = kind,
+            label = label,
+            documentation = documentation,
+            -- convert to traditional TextEdit
+            textEdit = {
+                newText = item.insertText,
+                range = item.range,
+            },
+            command = item.command,
+        }
+    end
 
     ---@type lsp.MultiHandler
     local function handler(responses)
         local items = {}
-        for client_id, rsp in pairs(responses) do
-            if not rsp.err and rsp.result and rsp.result.items and #rsp.result.items > 0 then
-                local item = rsp.result.items[1]
-                item.kind = vim.lsp.protocol.CompletionItemKind.Text
-                item.label = vim.trim(item.insertText)
-                item.documentation = string.format('```%s\n%s\n```\n', ft, item.label)
-                -- convert to traditional TextEdit
-                item.textEdit = {
-                    newText = item.insertText,
-                    range = item.range,
-                }
-
+        vim.iter(pairs(responses or {}))
+            :filter(function(_client_id, rsp)
+                return not rsp.err and rsp.result
+            end)
+            :each(function(client_id, rsp)
+                ---@type lsp.InlineCompletionItem[]|lsp.InlineCompletionList
+                local res = rsp.result
+                local inline_item = (vim.islist(res) and res or res.items)[1]
+                if not inline_item then
+                    return
+                end
+                local item = format_item(inline_item)
                 if self.opts.transform then
                     item = self.opts.transform(item, ctx, client_id)
                 end
                 table.insert(items, item)
-            end
-        end
+            end)
 
         callback({
             items = items,
@@ -76,7 +111,15 @@ function source:get_completions(ctx, callback)
             is_incomplete_backward = false,
         })
     end
-    local cancel = vim.lsp.buf_request_all(ctx.bufnr, vim.lsp.protocol.Methods.textDocument_inlineCompletion, params, handler)
+
+    local triggerKind = ctx.trigger.kind == 'Invoked' and TriggerKind.Invoked or TriggerKind.Automatic
+
+    local new_params = function(client, bufnr)
+        local params = vim.lsp.util.make_position_params(0, client.offset_encoding) --[[@as lsp.InlineCompletionParams]]
+        params.context = { triggerKind = triggerKind }
+        return self.opts.new_params(client, bufnr, params)
+    end
+    local cancel = vim.lsp.buf_request_all(ctx.bufnr, method, new_params, handler)
     return cancel
 end
 
