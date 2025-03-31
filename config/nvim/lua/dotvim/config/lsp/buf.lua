4e314ba90fd7306e2dba5a36d690cf04d74d2fbf
Xuyuan Pang <xuyuanp@gmail.com>
2025-03-31 10:28:02 +0800

    feat(nvim): removed unused border config

--- config/nvim/lua/dotvim/config/lsp/buf.lua
+++ config/nvim/lua/dotvim/config/lsp/buf.lua
@@ -1,32 +1,5 @@
 local buf = {}
 
-local function with_default_opts(f, default, n)
-    n = n or 1
-    default = default or {}
-    return function(...)
-        local params = { ... }
-        local opts = params[n] or {}
-        local default_opts = type(default) == 'function' and default() or default
-        params[n] = vim.tbl_deep_extend('force', default_opts, opts)
-        return f(unpack(params, 1, n))
-    end
-end
-
-local function floating_opts()
-    local max_width = math.ceil(vim.o.columns * 0.8) - 4
-    local width = math.min(80, max_width)
-    local default = {
-        width = width,
-        max_width = max_width,
-        title_pos = 'center',
-    }
-    return default
-end
-
-buf.signature_help = with_default_opts(vim.lsp.buf.signature_help, floating_opts)
-
-buf.hover = with_default_opts(vim.lsp.buf.hover, floating_opts)
-
 local code_action_backup = vim.lsp.buf.code_action
 ---@param opts? vim.lsp.buf.code_action.Opts
 function buf.code_action(opts)
@@ -38,25 +11,23 @@ function buf.code_action(opts)
     end
 end
 
-buf.outgoing_calls = with_default_opts(vim.lsp.buf.outgoing_calls)
-buf.incoming_calls = with_default_opts(vim.lsp.buf.incoming_calls)
-
-buf.format = with_default_opts(vim.lsp.buf.format)
-
 do
-    -- stylua: ignore
-    local methods = {
-        references       = 'lsp_references',
-        implementation   = 'lsp_implementations',
-        definition       = 'lsp_definitions',
-        type_definition  = 'lsp_type_definitions',
-        declaration      = 'lsp_declarations',
-        document_symbol  = 'lsp_symbols',
-        workspace_symbol = 'lsp_workspace_symbols',
-    }
-    for method, source in pairs(methods) do
-        buf[method] = function()
-            require('snacks.picker').pick(source)
+    local ok, picker = pcall(require, 'snacks.picker')
+    if ok then
+        -- stylua: ignore
+        local methods = {
+            references       = 'lsp_references',
+            implementation   = 'lsp_implementations',
+            definition       = 'lsp_definitions',
+            type_definition  = 'lsp_type_definitions',
+            declaration      = 'lsp_declarations',
+            document_symbol  = 'lsp_symbols',
+            workspace_symbol = 'lsp_workspace_symbols',
+        }
+        for method, source in pairs(methods) do
+            buf[method] = function()
+                picker.pick(source)
+            end
         end
     end
 end
