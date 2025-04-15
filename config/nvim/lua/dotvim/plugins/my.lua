94c105d441e24cf32790323973c3f0f40585a930
Xuyuan Pang <xuyuanp@gmail.com>
2025-04-15 14:42:17 +0800

    feat(nvim): updated nes config

--- config/nvim/lua/dotvim/plugins/my.lua
+++ config/nvim/lua/dotvim/plugins/my.lua
@@ -113,6 +113,27 @@ return {
     {
         'Xuyuanp/nes.nvim',
         event = 'VeryLazy',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
         opts = {},
+        keys = {
+            {
+                '<A-i>',
+                function()
+                    require('nes').get_suggestion()
+                end,
+                mode = 'i',
+                desc = '[Nes] get suggestion',
+            },
+            {
+                '<A-n>',
+                function()
+                    require('nes').apply_suggestion(0, { jump = true, trigger = true })
+                end,
+                mode = 'i',
+                desc = '[Nes] apply suggestion',
+            },
+        },
     },
 }
