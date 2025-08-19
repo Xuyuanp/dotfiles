6b424b3f6a5099d8e499baa534b5299655101eea
Xuyuan Pang <xuyuanp@gmail.com>
2025-08-19 17:37:34 +0800

    feat(nvim): updated markdown plugin configuration

--- config/nvim/lua/dotvim/plugins/langs.lua
+++ config/nvim/lua/dotvim/plugins/langs.lua
@@ -19,6 +19,11 @@ return {
                 icons = { ' ', ' ', ' ', ' ', ' ', ' ' },
                 right_pad = 1,
             },
+            win_options = {
+                conceallevel = {
+                    rendered = 1,
+                },
+            },
         },
         opts_extend = { 'file_types' },
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
