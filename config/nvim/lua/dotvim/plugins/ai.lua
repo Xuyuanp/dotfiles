de9ecc4d556f61789da1ff2c72c12ad778545a54
Xuyuan Pang <xuyuanp@gmail.com>
2025-08-19 17:37:13 +0800

    feat(nvim): updated mcp

--- config/nvim/lua/dotvim/plugins/ai.lua
+++ config/nvim/lua/dotvim/plugins/ai.lua
@@ -216,7 +216,6 @@ return {
         cmd = { 'MCPHub' },
         build = 'npm install -g mcp-hub@latest',
         opts = {
-            port = 3000,
             config = vim.fn.expand('~/.config/mcp/mcpservers.json'),
         },
         specs = {
@@ -227,18 +226,20 @@ return {
                     'ravitemer/mcphub.nvim',
                 },
                 opts = {
-                    strategies = {
-                        chat = {
-                            tools = {
-                                mcp = {
-                                    callback = function()
-                                        return require('mcphub.extensions.codecompanion')
-                                    end,
-                                    description = 'Call tools and resources from the MCP Servers',
-                                    opts = {
-                                        requires_approval = true,
-                                    },
-                                },
+                    extensions = {
+                        mcphub = {
+                            callback = 'mcphub.extensions.codecompanion',
+                            opts = {
+                                -- MCP Tools
+                                make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+                                show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+                                add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+                                show_result_in_chat = true, -- Show tool results directly in chat buffer
+                                format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+                                -- MCP Resources
+                                make_vars = true, -- Convert MCP resources to #variables for prompts
+                                -- MCP Prompts
+                                make_slash_commands = true, -- Add MCP prompts as /slash commands
                             },
                         },
                     },
