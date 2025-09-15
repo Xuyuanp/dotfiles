f823214218c072558c651cc932f3a95b53a74a18
Xuyuan Pang <xuyuanp@gmail.com>
2025-09-15 15:42:54 +0800

    feat(nvim): updated yamlls config

--- config/nvim/after/lsp/yamlls.lua
+++ config/nvim/after/lsp/yamlls.lua
@@ -2,13 +2,14 @@ return {
     settings = {
         yaml = {
             schemaStore = {
-                -- You must disable built-in schemaStore support if you want to use
-                -- this plugin and its advanced options like `ignore`.
                 enable = false,
-                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
                 url = '',
             },
             schemas = require('schemastore').yaml.schemas(),
+
+            format = {
+                enable = false, -- use yamlfmt of none-ls instead
+            },
         },
     },
 }
