b9365719da198e99ae20e14e0038c7361b08721d
Xuyuan Pang <xuyuanp@gmail.com>
2025-11-18 19:25:22 +0800

    feat(nvim): added new plugin nomad

--- config/nvim/lua/dotvim/plugins/tools.lua
+++ config/nvim/lua/dotvim/plugins/tools.lua
@@ -176,4 +176,17 @@ return {
         lazy = false,
         opts = {},
     },
+
+    {
+        'nomad/nomad',
+        version = '*',
+        cmd = { 'Mad' },
+        build = function()
+            ---@type nomad.neovim.build
+            local build = require('nomad.neovim.build')
+
+            build.builders.download_prebuilt():build(build.contexts.lazy())
+        end,
+        opts = {},
+    },
 }
