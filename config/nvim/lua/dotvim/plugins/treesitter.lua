4b14a135b06ce3146ebbded2cc0b074e73b0df3d
Xuyuan Pang <xuyuanp@gmail.com>
2026-03-23 10:38:33 +0800

    feat(nvim): lazy update

--- config/nvim/lua/dotvim/plugins/treesitter.lua
+++ config/nvim/lua/dotvim/plugins/treesitter.lua
@@ -58,14 +58,4 @@ return {
             },
         },
     },
-
-    {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        lazy = false,
-        init = function()
-            vim.g.skip_ts_context_commentstring_module = true
-        end,
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        opts = {},
-    },
 }
